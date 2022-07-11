48864f58239a28a0e61ed882e1801de14f35ff2a
e18264 <e18264@eng.pdn.ac.lk>
2022-07-12 02:45:37 +0530

    Complete dropdown | mastersPage, Add dummy 15 course frontmatters

--- assets/js/masters.js
+++ assets/js/masters.js
@@ -24,14 +24,20 @@ $(document).ready(() => {
     }
 
     $(function () {
-        let numberOItems = $(".card-content .card").length;
+        let numberOItemsAll = $(".card-content.all .card").length;
+        let numberOItemsCC = $(".card-content.cc .card").length;
+        let numberOItemsTE = $(".card-content.te .card").length;
+        let numberOItemsRT = $(".card-content.rt .card").length;
         let limitPerPage = 6;
-        let totalPages = Math.ceil(numberOItems / limitPerPage);
+        let totalPagesAll = Math.ceil(numberOItemsAll / limitPerPage);
+        let totalPagesCC = Math.ceil(numberOItemsCC / limitPerPage);
+        let totalPagesTE = Math.ceil(numberOItemsTE / limitPerPage);
+        let totalPagesRT = Math.ceil(numberOItemsRT / limitPerPage);
         let paginationSize = 7;
         let currentPage;
 
-        function showPage(whichPage) {
-            if (whichPage < 1 || whichPage > totalPages) return false;
+        function showPageAll(whichPage) {
+            if (whichPage < 1 || whichPage > totalPagesAll) return false;
 
             currentPage = whichPage;
 
@@ -41,20 +47,104 @@ $(document).ready(() => {
                 el.classList.remove("active");
             });
 
-            $(".card-content .card-boder").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show().toggleClass("active");
+            $(".card-content.all .card-boder").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show().toggleClass("active");
 
 
-            $(".card-content .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+            $(".card-content.all .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
 
             $(".pagination li").slice(1, -1).remove();
 
-            getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+            getPageList(totalPagesAll, currentPage, paginationSize).forEach(item => {
                 $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots").toggleClass("active", item === currentPage)
                     .append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text(item || "...")).insertBefore(".next-page");
             });
 
             $(".previous-page").toggleClass("disable", currentPage === 1);
-            $(".next-page").toggleClass("disable", currentPage === totalPages);
+            $(".next-page").toggleClass("disable", currentPage === totalPagesAll);
+            return true;
+        }
+
+        function showPageCC(whichPage) {
+            if (whichPage < 1 || whichPage > totalPagesCC) return false;
+
+            currentPage = whichPage;
+
+            let elems = document.querySelectorAll(".card-boder.active");
+
+            [].forEach.call(elems, function (el) {
+                el.classList.remove("active");
+            });
+
+            $(".card-content.cc .card-boder").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show().toggleClass("active");
+
+
+            $(".card-content.cc .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+            $(".pagination li").slice(1, -1).remove();
+
+            getPageList(totalPagesCC, currentPage, paginationSize).forEach(item => {
+                $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots").toggleClass("active", item === currentPage)
+                    .append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text(item || "...")).insertBefore(".next-page");
+            });
+
+            $(".previous-page").toggleClass("disable", currentPage === 1);
+            $(".next-page").toggleClass("disable", currentPage === totalPagesCC);
+            return true;
+        }
+
+        function showPageTE(whichPage) {
+            if (whichPage < 1 || whichPage > totalPagesTE) return false;
+
+            currentPage = whichPage;
+
+            let elems = document.querySelectorAll(".card-boder.active");
+
+            [].forEach.call(elems, function (el) {
+                el.classList.remove("active");
+            });
+
+            $(".card-content.te .card-boder").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show().toggleClass("active");
+
+
+            $(".card-content.te .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+            $(".pagination li").slice(1, -1).remove();
+
+            getPageList(totalPagesTE, currentPage, paginationSize).forEach(item => {
+                $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots").toggleClass("active", item === currentPage)
+                    .append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text(item || "...")).insertBefore(".next-page");
+            });
+
+            $(".previous-page").toggleClass("disable", currentPage === 1);
+            $(".next-page").toggleClass("disable", currentPage === totalPagesTE);
+            return true;
+        }
+
+        function showPageRT(whichPage) {
+            if (whichPage < 1 || whichPage > totalPagesRT) return false;
+
+            currentPage = whichPage;
+
+            let elems = document.querySelectorAll(".card-boder.active");
+
+            [].forEach.call(elems, function (el) {
+                el.classList.remove("active");
+            });
+
+            $(".card-content.rt .card-boder").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show().toggleClass("active");
+
+
+            $(".card-content.rt .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+            $(".pagination li").slice(1, -1).remove();
+
+            getPageList(totalPagesRT, currentPage, paginationSize).forEach(item => {
+                $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots").toggleClass("active", item === currentPage)
+                    .append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text(item || "...")).insertBefore(".next-page");
+            });
+
+            $(".previous-page").toggleClass("disable", currentPage === 1);
+            $(".next-page").toggleClass("disable", currentPage === totalPagesRT);
             return true;
         }
 
@@ -65,19 +155,69 @@ $(document).ready(() => {
                 .append($('<i class="fa fa-chevron-right"></i>')))
         );
 
-        $(".card-boder").show();
-        showPage(1);
+        $(".card-content.all .card-boder").show();
+        showPageAll(1);
 
         $(document).on("click", ".pagination li.current-page:not(.active)", function () {
-            return showPage(+$(this).text());
+            if (document.querySelector(".dropdown-toggle label.selection").innerText === "All") {
+                return showPageAll(+$(this).text());
+            } else if (document.querySelector(".dropdown-toggle label.selection").innerText === "Core Courses") {
+                return showPageCC(+$(this).text());
+            } else if (document.querySelector(".dropdown-toggle label.selection").innerText === "Technical Electives") {
+                return showPageTE(+$(this).text());
+            } else {
+                return showPageRT(+$(this).text());
+            }
         });
 
         $(".next-page").on("click", function () {
-            return showPage(currentPage + 1);
+            if (document.querySelector(".dropdown-toggle label.selection").innerText === "All") {
+                return showPageAll(currentPage + 1);
+            } else if (document.querySelector(".dropdown-toggle label.selection").innerText === "Core Courses") {
+                return showPageCC(currentPage + 1);
+            } else if (document.querySelector(".dropdown-toggle label.selection").innerText === "Technical Electives") {
+                return showPageTE(currentPage + 1);
+            } else {
+                return showPageRT(currentPage + 1);
+            }
         });
 
         $(".previous-page").on("click", function () {
-            return showPage(currentPage - 1);
+            if (document.querySelector(".dropdown-toggle label.selection").innerText === "All") {
+                return showPageAll(currentPage - 1);
+            } else if (document.querySelector(".dropdown-toggle label.selection").innerText === "Core Courses") {
+                return showPageCC(currentPage - 1);
+            } else if (document.querySelector(".dropdown-toggle label.selection").innerText === "Technical Electives") {
+                return showPageTE(currentPage - 1);
+            } else {
+                return showPageRT(currentPage - 1);
+            }
         });
+
+        document.querySelectorAll(".dropdown-item label").forEach(el => {
+            el.addEventListener('click', () => {
+                document.querySelector(".dropdown-toggle label.selection").innerText = el.innerText;
+
+                $(".card-content.all .card-boder").hide();
+                $(".card-content.cc .card-boder").hide();
+                $(".card-content.te .card-boder").hide();
+                $(".card-content.rt .card-boder").hide();
+
+                if (el.innerText === "All") {
+                    $(".card-content.all .card-boder").show();
+                    showPageAll(1);
+                } else if (el.innerText === "Core Courses") {
+                    $(".card-content.cc .card-boder").show();
+                    showPageCC(1);
+                } else if (el.innerText === "Technical Electives") {
+                    $(".card-content.te .card-boder").show();
+                    showPageTE(1);
+                } else {
+                    $(".card-content.rt .card-boder").show();
+                    showPageRT(1);
+                }
+            });
+        });
+
     });
 });
