3af0d850863a8c8c29273820c59aa8c5d6f17ac3
e18264 <e18264@eng.pdn.ac.lk>
2022-07-17 22:38:31 +0530

    Add reveal effect on scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,24 @@ $(document).ready(function($) {
         delay: 10,
         time: 1000
     });
+
+    window.addEventListener('scroll', reveal);
+
+    function reveal(){
+        let reveals = document.querySelectorAll('.reveal');
+
+        for(let i=0; i<reveals.length; i++){
+            let windowheight = window.innerHeight;
+            let revealtop = reveals[i].getBoundingClientRect().top;
+            let revealpoint = 150;
+
+            if(revealtop < windowheight - revealpoint){
+                reveals[i].classList.add('active');
+            } 
+            // else {
+            //     reveals[i].classList.remove('active');
+            // }
+        }
+    }
+
 });
