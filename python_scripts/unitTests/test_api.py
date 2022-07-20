d2e6d6c7a397f26ad8b798bb0ef5bd3681712e71
unknown <shehan2k@gmail.com>
2022-07-21 03:06:37 +0530

    Add github actions

--- python_scripts/unitTests/test_api.py
+++ python_scripts/unitTests/test_api.py
@@ -6,6 +6,7 @@ def json_validation(filename):
         data = f.read()
     try:
         json.loads(data)
+        print("Valid json syntax")
         return True
     except ValueError as err:
         print(err)
@@ -15,7 +16,7 @@ def json_validation(filename):
 class Test_API(unittest.TestCase):
       
     def setUp(self):
-        self.api_path = "../_site/api/courses/index.json"
+        self.api_path = "../../_site/api/courses/index.json"
     
     def test_api_json_syntax(self):
         self.assertTrue(json_validation(self.api_path), "Invalid json syntax in API")
